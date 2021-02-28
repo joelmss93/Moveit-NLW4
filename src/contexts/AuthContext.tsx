a910095d08f6535b2e3d11d2fdc43dc43cfd9e30
joelmss93 <joelmss93@gmail.com>
2021-02-28 12:44:25 -0300

    Initiated Auth

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, FormEvent, useCallback } from 'react';
+import {
+  Provider,
+  signOut as handleLogout,
+  signIn as handleLogin,
+} from 'next-auth/client';
+
+export interface AuthContextData {
+  signIn: (event: FormEvent) => void;
+  signOut: (event: FormEvent) => void;
+}
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+  pageProps: any;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({
+  pageProps,
+  children,
+}: AuthProviderProps): JSX.Element {
+  const signIn = useCallback((event: FormEvent) => {
+    event.preventDefault();
+    handleLogin('github', { callbackUrl: process.env.DEV_URL });
+  }, []);
+
+  const signOut = useCallback((event: FormEvent) => {
+    event.preventDefault();
+    handleLogout({ callbackUrl: process.env.NEXTAUTH_URL });
+  }, []);
+
+  return (
+    <Provider session={pageProps}>
+      <AuthContext.Provider value={{ signIn, signOut }}>
+        {children}
+      </AuthContext.Provider>
+    </Provider>
+  );
+}
